a8b4183ab3aa2f1844062ca90f0dfca29dce65ac
AntonioJesusRG <antoniogar129@gmail.com>
2023-02-13 19:33:38 +0100

    Fix describe error in the header test file

--- src/core/components/header/header.test.tsx
+++ src/core/components/header/header.test.tsx
@@ -3,7 +3,7 @@ import { Header } from "./header";
 
 describe("Given Header component", () => {
   describe("When it is render", () => {
-    test("Then it should have the footer in the screen", () => {
+    test("Then it should be an image in the document", () => {
       render(<Header></Header>);
       const element = screen.getByRole("img");
       expect(element).toBeInTheDocument();
