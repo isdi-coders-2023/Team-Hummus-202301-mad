85c79e78065a97da5d6d880c5f5996d696f4294e
Jimena <jimenanrp2@gmail.com>
2023-02-13 17:00:38 +0100

    Organize files and add footer component

---
+++ src/core/components/footer/footer.tsx
@@ -0,0 +1,11 @@
+export function Footer () {
+  return (
+    <footer>
+      <div>
+        <img src="" alt="Rick and Morty Logo"/>
+          <p>Copyright © Adult Swim
+            All rights reserved</p>
+      </div>
+    </footer>
+  )
+};
