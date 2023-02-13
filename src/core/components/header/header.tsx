84cbcf3d898ae4c232e691751188a713cd406729
AntonioJesusRG <antoniogar129@gmail.com>
2023-02-13 18:28:18 +0100

    Edit header component

--- src/core/components/header/header.tsx
+++ src/core/components/header/header.tsx
@@ -1,3 +1,12 @@
+import "./header.scss";
+
 export function Header() {
-  return;
+  return (
+    <header className="app-header">
+      <div className="app-header__logo">
+        <img src="images\logo_rick_and_morty.svg" alt="logo" />
+      </div>
+      <div className="app-header__menu">Home Favourites About</div>
+    </header>
+  );
 }
