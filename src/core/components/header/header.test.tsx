f8f72cf7f0733560367232d80481ec1b2e863b4c
AntonioJesusRG <antoniogar129@gmail.com>
2023-02-13 19:27:09 +0100

    Fix describe error

--- src/core/components/header/header.test.tsx
+++ src/core/components/header/header.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { Header } from "./header";
 
-describe("Given Footer component", () => {
+describe("Given Header component", () => {
   describe("When it is render", () => {
     test("Then it should have the footer in the screen", () => {
       render(<Header></Header>);
