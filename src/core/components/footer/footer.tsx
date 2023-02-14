d32defdf879e267d0da98828943ad424d699c241
Jimena <jimenanrp2@gmail.com>
2023-02-14 11:10:00 +0100

    📝  Change footer JSX

--- src/core/components/footer/footer.tsx
+++ src/core/components/footer/footer.tsx
@@ -1,11 +1,32 @@
-export function Footer () {
+import "./footer.scss";
+
+export function Footer() {
   return (
-    <footer>
-      <div>
-        <img src="" alt="Rick and Morty Logo"/>
-          <p>Copyright © Adult Swim
-            All rights reserved</p>
+    <footer className="footer">
+      <div className="footer__logo">
+        <img src="./img/logo_rick_and_morty.svg" alt="Rick and Morty Logo" />
+        <div className="footer__p">
+          <p>Copyright © Adult Swim </p>
+          <p>All rights reserved</p>
+        </div>
+      </div>
+      <div className="footer__hummus">
+        <p className="footer__hummus-title">By Hummus team</p>
+        <div className="footer__names">
+          <div>
+            <img src="" alt="" />
+            <p>Jimena</p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <p>Antonio</p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <p>Joaquin</p>
+          </div>
+        </div>
       </div>
     </footer>
-  )
-};
+  );
+}
