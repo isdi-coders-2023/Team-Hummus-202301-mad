a2fe8de935018b3f1d710db0b1d791da66f18fb4
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-02-14 21:50:18 +0100

    fixed navbar test

--- src/core/components/navbar/navbar.test.tsx
+++ src/core/components/navbar/navbar.test.tsx
@@ -1,16 +1,23 @@
 import { render, screen } from "@testing-library/react";
-import { BrowserRouter, Router } from "react-router-dom";
 import { Navbar } from "./navbar";
+import { MenuOption } from "../app/App";
+import { MemoryRouter as Router } from "react-router-dom";
 
 describe("Given Navbar component", () => {
   describe("When it is rendered", () => {
-    test("Then menu should be in the screen", () => {
+    test("Then it should render in the header component", () => {
+      const mockOptions: MenuOption[] = [
+        {
+          label: "test",
+          path: "/test",
+        },
+      ];
       render(
-        <BrowserRouter location={""} navigator={undefined}>
-          <Navbar></Navbar>
-        </BrowserRouter>
+        <Router>
+          <Navbar menuOptions={mockOptions}></Navbar>
+        </Router>
       );
-      const element = screen.getByText(/Home/i);
+      const element = screen.getByText(mockOptions[0].label);
       expect(element).toBeInTheDocument();
     });
   });
--- src/core/components/navbar/navbar.tsx
+++ src/core/components/navbar/navbar.tsx
@@ -2,14 +2,14 @@ import "./navbar.scss";
 import { MenuOption } from "../app/App";
 
 type NavProps = {
-  options: MenuOption[];
+  menuOptions: MenuOption[];
 };
-export function Navbar({ options }: NavProps) {
+export function Navbar({ menuOptions }: NavProps) {
   return (
     <nav>
       <img className="burger-menu" src="/img/menu.png" alt="" />
       <ul className="navbar-list">
-        {options.map((item) => (
+        {menuOptions.map((item) => (
           <li key={item.label} className="navbar-list__home">
             <a href={item.path}>{item.label}</a>
           </li>
