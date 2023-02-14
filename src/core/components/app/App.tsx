2d11d2be656457e35916fcfe70808bac8b114884
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-02-14 18:17:10 +0100

    add routes and browser router

--- src/core/components/app/App.tsx
+++ src/core/components/app/App.tsx
@@ -2,13 +2,21 @@ import React from "react";
 import { Home } from "../../../features/home/home";
 import { Footer } from "../footer/footer";
 import { Header } from "../header/header";
+import { Route } from "react-router-dom";
 import "./App.scss";
+import { About } from "../../../features/about/about";
+import { Instructions } from "../../../features/how.to.use/instructions";
+import { Routes } from "react-router";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/howto" element={<Instructions></Instructions>}></Route>
+      </Routes>
       <Footer></Footer>
     </div>
   );
