b58b26e3f003559cb702e832c18deb7d64510fe0
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-02-14 18:57:22 +0100

    add routes and links to navbar elements

--- src/core/components/navbar/navbar.tsx
+++ src/core/components/navbar/navbar.tsx
@@ -1,5 +1,5 @@
 import "./navbar.scss";
-
+import { Link } from "react-router-dom";
 export const Navbar = () => {
   return (
     <nav>
@@ -12,10 +12,18 @@ export const Navbar = () => {
 const NavbarList = () => {
   return (
     <ul className="navbar-list">
-      <li className="navbar-list__home">Home</li>
-      <li className="navbar-list__howto">How to</li>
-      <li className="navbar-list__about">About</li>
-      <li className="navbar-list__favorites">Favorites</li>
+      <li className="navbar-list__home">
+        <Link to="/">Home</Link>
+      </li>
+      <li className="navbar-list__howto">
+        <Link to="/howto">How to</Link>
+      </li>
+      <li className="navbar-list__about">
+        <Link to="/about">About</Link>
+      </li>
+      <li className="navbar-list__favorites">
+        <Link to="/favorites">Favorites</Link>
+      </li>
     </ul>
   );
 };
