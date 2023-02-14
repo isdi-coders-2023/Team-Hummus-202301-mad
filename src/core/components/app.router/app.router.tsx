2e9da0c16c1dda7fedd4d6890fab3d6ad23b9ecc
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-02-14 21:21:03 +0100

    add app.router and changed page functions to default

---
+++ src/core/components/app.router/app.router.tsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import { MenuOption } from "../app/App";
+import { Suspense, lazy } from "react";
+const Home = lazy(() => import("../../../features/home/home"));
+const About = lazy(() => import("../../../features/about/about"));
+const HowTo = lazy(() => import("../../../features/how.to.use/instructions"));
+/* const Favorites = lazy(() => import("../../../features/about/about"));*/
+
+type AppRouterProps = {
+  menuOptions: MenuOption[];
+};
+
+export function AppRouter({ menuOptions }: AppRouterProps) {
+  return (
+    <Suspense>
+      <Routes>
+        <Route path={"/"} element={<Home></Home>}></Route>
+        <Route path={menuOptions[0].path} element={<Home></Home>}></Route>
+        <Route path={menuOptions[1].path} element={<About></About>}></Route>
+        <Route path={menuOptions[2].path} element={<HowTo></HowTo>}></Route>
+      </Routes>
+    </Suspense>
+  );
+}
