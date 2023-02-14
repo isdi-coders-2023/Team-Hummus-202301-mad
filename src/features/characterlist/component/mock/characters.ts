95bca56996fb6f829ee8bde7274763ac62e0ed4f
AntonioJesusRG <antoniogar129@gmail.com>
2023-02-14 12:59:55 +0100

    Add mock with characters

---
+++ src/features/characterlist/component/mock/characters.ts
@@ -0,0 +1,16 @@
+import { CharacterStructure } from "../model/character";
+
+export const MOCK_Characters: CharacterStructure[] = [
+  {
+    name: "Pepe",
+    img: "https://rickandmortyapi.com/api/character/avatar/361.jpeg",
+  },
+  {
+    name: "Paco",
+    img: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+  },
+  {
+    name: "Pepo",
+    img: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+  },
+];
