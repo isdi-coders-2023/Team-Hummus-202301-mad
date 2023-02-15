a171639852db337ba2ad8638ff24346fd22941f2
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-02-15 16:23:59 +0100

    Add error, error test and error scss

---
+++ src/features/error/error.tsx
@@ -0,0 +1,10 @@
+export default function Error() {
+  return (
+    <div className="error-section">
+      <h2 className="error-section__msg">Error!</h2>
+      <p className="error-section__msg">Page not found</p>
+      <img src="" alt="" />
+      <p className="error-section__click">Click here to head back home</p>
+    </div>
+  );
+}
