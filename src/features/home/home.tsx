06b2e1fd3ad12d3b6aa80da8cd30a23003a65662
AntonioJesusRG <antoniogar129@gmail.com>
2023-02-15 18:03:35 +0100

    Add component characters list inside home

--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -1,5 +1,11 @@
+import { CharacterList } from "../characterlist/component/characterlist/characterlist";
 import { Cover } from "../cover/cover";
 
 export default function Home() {
-  return <Cover></Cover>;
+  return (
+    <>
+      <Cover></Cover>
+      <CharacterList></CharacterList>
+    </>
+  );
 }
