3abcee1b8db5892008b4dac709cd6b5a41c7bede
AntonioJesusRG <antoniogar129@gmail.com>
2023-02-15 17:07:03 +0100

    Modify test

--- src/features/characterlist/component/character/character.test.tsx
+++ src/features/characterlist/component/character/character.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+//import userEvent from "@testing-library/user-event";
 import { CharacterStructure } from "../model/character";
 import { Character } from "./character";
 
@@ -13,5 +14,17 @@ describe("Given a character", () => {
       const element = screen.getAllByRole("button");
       expect(element[0]).toBeInTheDocument();
     });
+    // Parte del test que no se puede hacer hasta tener contexto
+    /*   test("then if user clic on fav button it should  handle function", async () => {
+      render(<Character char={mockChar}></Character>);
+      const handleFav = jest.fn();
+      const element = screen.getAllByRole("button", {
+        hidden: true,
+      });
+
+      userEvent.click(element[0]);
+      console.log(element[0]);
+      expect(handleFav).toBeCalled();
+    }); */
   });
 });
