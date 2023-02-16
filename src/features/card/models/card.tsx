0cf59fc7a00db95d7dfda33ca6c5c3ed7a7ec9aa
Jimena <jimenanrp2@gmail.com>
2023-02-16 12:46:05 +0100

    Edit CardStructure

--- src/features/card/models/card.tsx
+++ src/features/card/models/card.tsx
@@ -1,15 +1,32 @@
 type isFavourite = {
-  isFavourite: boolean;
+  id: number;
 };
 
 export type ProtoCardStructure = {
-  id: number;
+  image: string;
   name: string;
   status: string;
   species: string;
   type: string;
   gender: string;
   location: string;
-}
+  isFavourite: boolean;
+};
 
-export type CardStructure = isFavourite & ProtoCardStructure
+export type CardStructure = isFavourite & ProtoCardStructure;
+
+// export class ProtoCard implements ProtoCardStructure {
+//   isFavourite: boolean;
+
+//   constructor(
+//     public image: string,
+//     public name: string,
+//     public status: string,
+//     public species: string,
+//     public type: string,
+//     public gender: string,
+//     public location: string
+//   ) {
+//     this.isFavourite = false;
+//   }
+// }
