3f26f94b6382f21eb2bd36c0a036f9ca64215ce8
AntonioJesusRG <antoniogar129@gmail.com>
2023-02-16 22:01:46 +0100

    Change cardlist component

--- src/components/cardlist/cardlist.tsx
+++ src/components/cardlist/cardlist.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect } from "react";
 import { AppContext } from "../../context/context";
+import { CharacterStructure } from "../../models/character/character";
 import { Card } from "../card/card";
 
 import "./cardlist.scss";
@@ -7,14 +8,14 @@ import "./cardlist.scss";
 export function CharacterList() {
   const { chars, loadChars } = useContext(AppContext);
 
-  useEffect(() => {
+  /*  useEffect(() => {
     loadChars();
-  }, [loadChars]);
+  }, [loadChars]); */
 
   return (
     <section className="character-list">
       <ul className="character-list__character">
-        {chars.map((item) => (
+        {chars?.map((item: CharacterStructure) => (
           <Card key={item.id} char={item}></Card>
         ))}
       </ul>
