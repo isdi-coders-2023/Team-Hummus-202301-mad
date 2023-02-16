9910bb1b95a5af317cc30dc18c6dbf26186bdee0
Jimena <jimenanrp2@gmail.com>
2023-02-16 18:01:25 +0100

    Modify JSX from 'add.character'

--- src/features/add.character/page/add.character.tsx
+++ src/features/add.character/page/add.character.tsx
@@ -1,10 +1,11 @@
 import { Form } from "../../forms/page/form";
+import "./add.character.scss";
 
-export default function AddCharacter() {
+export default function Add() {
   return (
-    <>
+    <div className="add-div">
       <h2>Add new character</h2>
       <Form url="http://localhost:3000/add"></Form>
-    </>
+    </div>
   );
 }
