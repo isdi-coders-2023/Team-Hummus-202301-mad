8dcbac7250ca96b2f8db73a86d24a3e5414ce005
Jimena <jimenanrp2@gmail.com>
2023-02-16 18:01:55 +0100

    Edit form file

--- src/features/forms/page/form.tsx
+++ src/features/forms/page/form.tsx
@@ -2,8 +2,7 @@ import { SyntheticEvent } from "react";
 import { ProtoCardStructure } from "../../card/models/card";
 import "./form.scss";
 
-export function Form({url}) {
-
+export function Form({ url }: any) {
   // const { updateTask, createTask } = useContext(AppContext);
 
   const handleSubmit = (event: SyntheticEvent) => {
@@ -21,28 +20,44 @@ export function Form({url}) {
       isFavourite: true,
     };
 
+    console.log(newCard);
     // Hay que poner esto de abajo cuando este creada la api privada y la ruta del add.
     // url === 'http://localhost:3000/add'? CreateCard(newCard) : UpdateCard(newCard);
-
   };
 
   return (
     <form action="" onSubmit={handleSubmit}>
-      <input type="text" placeholder="https://www.example.png" />
-      <label htmlFor="">Image: {}</label>
-      <input type="text" placeholder="1" />
-      <label htmlFor="">Name: </label>
-      <input type="text" placeholder="1" />
-      <label htmlFor="">Status: </label>
-      <input type="text" placeholder="1" />
-      <label htmlFor="">Species: </label>
-      <input type="text" placeholder="1" />
-      <label htmlFor="">Type: </label>
-      <input type="text" placeholder="1" />
-      <label htmlFor="">Gender: </label>
-      <input type="text" placeholder="1" />
-      <label htmlFor="">Location: </label>
-      <button type="submit">Add</button>
+      <div className="form-line">
+        <label htmlFor="">Image url: </label>
+        <input type="text" placeholder="https://www.example.png" />
+      </div>
+      <div className="form-line">
+        <label htmlFor="">Name: </label>
+        <input type="text" placeholder="Rick" />
+      </div>
+      <div className="form-line">
+        <label htmlFor="">Status: </label>
+        <input type="text" placeholder="alive" />
+      </div>
+      <div className="form-line">
+        <label htmlFor="">Species: </label>
+        <input type="text" placeholder="human" />
+      </div>
+      <div className="form-line">
+        <label htmlFor="">Type: </label>
+        <input type="text" placeholder="-" />
+      </div>
+      <div className="form-line">
+        <label htmlFor="">Gender: </label>
+        <input type="text" placeholder="male" />
+      </div>
+      <div className="form-line">
+        <label htmlFor="">Location: </label>
+        <input type="text" placeholder="earth" />
+      </div>
+      <div className="button-div">
+        <button type="submit">ADD</button>
+      </div>
     </form>
   );
 }
