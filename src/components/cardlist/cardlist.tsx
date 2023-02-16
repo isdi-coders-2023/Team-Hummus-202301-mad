f14433f29491f73c9c2afc2682a3cd9c87195271
AntonioJesusRG <antoniogar129@gmail.com>
2023-02-16 20:29:17 +0100

    Add context to cardlist

--- src/components/cardlist/cardlist.tsx
+++ src/components/cardlist/cardlist.tsx
@@ -1,17 +1,21 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AppContext } from "../../context/context";
 import { Card } from "../card/card";
 
 import "./cardlist.scss";
 
 export function CharacterList() {
-  const { chars } = useContext(AppContext);
+  const { chars, loadChars } = useContext(AppContext);
+
+  useEffect(() => {
+    loadChars();
+  }, [loadChars]);
 
   return (
     <section className="character-list">
       <ul className="character-list__character">
-        {chars.map((item, i) => (
-          <Card key={i++} char={item}></Card>
+        {chars.map((item) => (
+          <Card key={item.id} char={item}></Card>
         ))}
       </ul>
     </section>
