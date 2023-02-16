914602ce80726d9511e1a655970d5ee6c53f4ddf
AntonioJesusRG <antoniogar129@gmail.com>
2023-02-16 16:06:33 +0100

    Change mock for context

--- src/components/cardlist/cardlist.tsx
+++ src/components/cardlist/cardlist.tsx
@@ -1,13 +1,16 @@
-import { MOCK_Cards } from "../../mocks/cards";
+import { useContext } from "react";
+import { AppContext } from "../../context/context";
 import { Card } from "../card/card";
 
 import "./cardlist.scss";
 
 export function CharacterList() {
+  const { chars } = useContext(AppContext);
+
   return (
     <section className="character-list">
       <ul className="character-list__character">
-        {MOCK_Cards.map((item, i) => (
+        {chars.map((item, i) => (
           <Card key={i++} char={item}></Card>
         ))}
       </ul>
