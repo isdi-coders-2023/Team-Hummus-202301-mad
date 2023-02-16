11dddccc856f84cfb60a6c4bbb67b7fc0e725600
Jimena <jimenanrp2@gmail.com>
2023-02-16 18:04:15 +0100

    Erase 'add' path from AppRouter

--- src/core/components/app.router/app.router.tsx
+++ src/core/components/app.router/app.router.tsx
@@ -4,9 +4,9 @@ import { Suspense, lazy } from "react";
 const Home = lazy(() => import("../../../features/home/home"));
 const About = lazy(() => import("../../../features/about/about"));
 const HowTo = lazy(() => import("../../../features/how.to.use/instructions"));
-const Add = lazy(() => import("../../../features/add.character/page/add.character")
-);
-/* const Favorites = lazy(() => import("../../../features/favorites/favorites"));*/
+
+/* const Favorites = lazy(() => import("../../../features/favorites/favorites"));
+const Add = lazy(() => import("../../../features/add.character/page/add.character")); */
 
 type AppRouterProps = {
   menuOptions: MenuOption[];
@@ -19,7 +19,6 @@ export function AppRouter({ menuOptions }: AppRouterProps) {
         <Route path={"/"} element={<Home></Home>}></Route>
         <Route path={menuOptions[1].path} element={<About></About>}></Route>
         <Route path={menuOptions[2].path} element={<HowTo></HowTo>}></Route>
-        <Route path={menuOptions[3].path} element={<Add></Add>}></Route>
       </Routes>
     </Suspense>
   );
