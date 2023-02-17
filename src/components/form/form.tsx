a34a5484a6b5d81f7df663db1f6299e951579385
Jimena <jimenanrp2@gmail.com>
2023-02-17 12:38:12 +0100

    Erase console log from code

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -20,40 +20,39 @@ export function Form({ url }: any) {
       isFavourite: true,
     };
 
-    console.log(newCharacter);
     // Hay que poner esto de abajo cuando este creada la api privada y la ruta del add.
-    // url === 'http://localhost:3000/add'? CreateCard(newCard) : UpdateCard(newCard);
+    //  Esto -> url === 'http://localhost:3000/add'? CreateCard(newCharacter) : UpdateCard(newCharacter);
   };
 
   return (
-    <form action="" onSubmit={handleSubmit}>
+    <form action="" onSubmit={handleSubmit} id="form">
       <div className="form-line">
         <label htmlFor="">Image url: </label>
-        <input type="text" placeholder="https://www.example.png" />
+        <input type="text" placeholder="https://www.example.png" required />
       </div>
       <div className="form-line">
         <label htmlFor="">Name: </label>
-        <input type="text" placeholder="Rick" />
+        <input type="text" placeholder="Rick" required />
       </div>
       <div className="form-line">
         <label htmlFor="">Status: </label>
-        <input type="text" placeholder="alive" />
+        <input type="text" placeholder="alive" required />
       </div>
       <div className="form-line">
         <label htmlFor="">Species: </label>
-        <input type="text" placeholder="human" />
+        <input type="text" placeholder="human" required />
       </div>
       <div className="form-line">
         <label htmlFor="">Type: </label>
-        <input type="text" placeholder="-" />
+        <input type="text" placeholder="-" required />
       </div>
       <div className="form-line">
         <label htmlFor="">Gender: </label>
-        <input type="text" placeholder="male" />
+        <input type="text" placeholder="male" required />
       </div>
       <div className="form-line">
         <label htmlFor="">Location: </label>
-        <input type="text" placeholder="earth" />
+        <input type="text" placeholder="earth" required />
       </div>
       <div className="button-div">
         <button type="submit">ADD</button>
