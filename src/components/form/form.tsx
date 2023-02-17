f994ec502904090bd0367af4e6218ab02be34df5
Jimena <jimenanrp2@gmail.com>
2023-02-17 15:58:19 +0100

    Edit 'form'

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,16 +1,23 @@
 import { SyntheticEvent, useContext } from "react";
-import { ProtoCharacterStructure } from "../../models/character/character";
+import {
+  CharacterStructure,
+  ProtoCharacterStructure,
+} from "../../models/character/character";
 import "./form.scss";
 import { AppContextPrivate } from "../../context private/context.private";
 
-export function Form({ url }: any) {
+export function Form({
+  char
+}: {
+  char: CharacterStructure | ProtoCharacterStructure;
+}) {
   const { updateChar, addChar } = useContext(AppContextPrivate);
 
   const handleSubmit = (event: SyntheticEvent) => {
     event.preventDefault();
     const form = event.target as HTMLFormElement;
     const inputs = form.querySelectorAll("input");
-    const newCharacter: ProtoCharacterStructure = {
+    const newCharacter: ProtoCharacterStructure | CharacterStructure = {
       image: inputs[0].value,
       name: inputs[1].value,
       status: inputs[2].value,
@@ -19,32 +26,40 @@ export function Form({ url }: any) {
       gender: inputs[5].value,
       location: inputs[6].value,
       isFavourite: true,
-    };
+    } as ProtoCharacterStructure;
+    const type = char === null ? "add" : "";
+    char === null &&
+      (char = {
+        image: "",
+        name: "",
+        status: "",
+        species: "",
+        type: "",
+        gender: "",
+        location: "",
+        isFavourite: true,
+      }as ProtoCharacterStructure);
 
-    // const updateCharacter: CharacterStructure = {
-    //   id: inputs[0].value, // Cambiar
-    //   image: inputs[0].value,
-    //   name: inputs[1].value,
-    //   status: inputs[2].value,
-    //   species: inputs[3].value,
-    //   type: inputs[4].value,
-    //   gender: inputs[5].value,
-    //   location: inputs[6].value,
-    //   isFavourite: true,
-    // };
-
-    console.log("add character", newCharacter);
-    addChar(newCharacter);
-    // url === "http://localhost:3000/add"
-    //   ? addChar(newCharacter)
-    // : updateChar(newCharacter);
+    // console.log("add character", newCharacter);
+    // addChar(newCharacter);
+    if (type === "add") {
+      addChar(newCharacter);
+    } else {
+      newCharacter.id = char.id;
+      updateChar(newCharacter);
+    }
   };
 
   return (
     <form action="" onSubmit={handleSubmit} id="form">
       <div className="form-line">
         <label htmlFor="">Image url: </label>
-        <input type="text" placeholder="https://www.example.png" required />
+        <input
+          type="text"
+          placeholder="https://www.example.png"
+          value={char.image}
+          required
+        />
       </div>
       <div className="form-line">
         <label htmlFor="">Name: </label>
