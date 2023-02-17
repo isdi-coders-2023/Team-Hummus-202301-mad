dd238becbf4389f5602602680877ec2e0aa3b40b
Jimena <jimenanrp2@gmail.com>
2023-02-17 12:38:44 +0100

    Edit form test

--- src/components/form/form.test.tsx
+++ src/components/form/form.test.tsx
@@ -1,5 +1,11 @@
 /* eslint-disable testing-library/no-render-in-setup */
-import { render, screen } from "@testing-library/react";
+import {
+  fireEvent,
+  getByRole,
+  getByTestId,
+  render,
+  screen,
+} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Form } from "./form";
 
@@ -8,9 +14,18 @@ describe("Given the From component ", () => {
 
   beforeEach(() => {
     jest.fn().mockReturnValue({ name: "test" });
-    render(<Form></Form>);
+    render(<Form/>);
 
     elements = [...screen.getAllByRole("textbox"), screen.getByRole("button")];
+    const mockReturnValue = [
+      { value: "https://www.example.png" },
+      { value: "Rick" },
+      { value: "alive" },
+      { value: "human" },
+      { value: "-" },
+      { value: "male" },
+      { value: "earth" },
+    ];
   });
 
   describe("When we render it", () => {
@@ -25,6 +40,13 @@ describe("Given the From component ", () => {
       const mockUser = "Rick";
       userEvent.type(elements[1], mockUser);
       expect(elements[1]).toHaveValue(mockUser);
+    });
+  });
+
+  describe("When the user submits the form...", () => {
+    test("submits the form with the correct ProtoCharacterStructure object", () => {
+      userEvent.click(screen.getByRole('button'))
+      // expect(onSubmit).toBeCalled();
     });
   });
 });
