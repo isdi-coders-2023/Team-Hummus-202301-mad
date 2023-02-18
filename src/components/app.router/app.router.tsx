33f159efa0a4c6ae1b07dc61f073773024150611
AntonioJesusRG <antoniogar129@gmail.com>
2023-02-18 20:05:07 +0100

    Change approuter

--- src/components/app.router/app.router.tsx
+++ src/components/app.router/app.router.tsx
@@ -5,9 +5,6 @@ const Home = lazy(() => import("../../pages/home/home"));
 const About = lazy(() => import("../../pages/about/about"));
 const HowTo = lazy(() => import("../../pages/how.to.use/instructions"));
 const Favourites = lazy(() => import("../../pages/favourites/favourites"));
-const Add = lazy(() => import("../../pages/add/add.character"));
-const Detail = lazy(() => import("../../pages/detail/detail"));
-const Error = lazy(() => import("../../pages/error/error"));
 
 type AppRouterProps = {
   menuOptions: MenuOption[];
@@ -20,14 +17,10 @@ export function AppRouter({ menuOptions }: AppRouterProps) {
         <Route path={"/"} element={<Home></Home>}></Route>
         <Route path={menuOptions[1].path} element={<About></About>}></Route>
         <Route path={menuOptions[2].path} element={<HowTo></HowTo>}></Route>
-
         <Route
           path={menuOptions[3].path}
           element={<Favourites></Favourites>}
         ></Route>
-        <Route path={menuOptions[4].path} element={<Add></Add>}></Route>
-        <Route path={menuOptions[5].path} element={<Detail></Detail>}></Route>
-        <Route path={menuOptions[6].path} element={<Error></Error>}></Route>
       </Routes>
     </Suspense>
   );
