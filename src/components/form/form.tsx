46727cb99627b5b058a73803b209cc9a358c2e1d
Jimena <jimenanrp2@gmail.com>
2023-02-18 21:34:20 +0100

    Comment code lines to check an experiment

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -7,7 +7,7 @@ import "./form.scss";
 import { AppContextPrivate } from "../../context/context private/context.private";
 
 export function Form({ char, onSubmit }: { char: CharacterStructure | ProtoCharacterStructure | null, onSubmit: any }) {
-  const { updateChar, addChar } = useContext(AppContextPrivate);
+  // const { updateChar, addChar } = useContext(AppContextPrivate);
 
   const type = char === null ? "add" : "";
   char === null &&
@@ -22,27 +22,27 @@ export function Form({ char, onSubmit }: { char: CharacterStructure | ProtoChara
       isFavourite: true,
     } as ProtoCharacterStructure);
 
-  const handleSubmit = (event: SyntheticEvent) => {
-    event.preventDefault();
-    const form = event.target as HTMLFormElement;
-    const inputs = form.querySelectorAll("input");
-    const newCharacter: ProtoCharacterStructure | CharacterStructure = {
-      image: inputs[0].value,
-      name: inputs[1].value,
-      status: inputs[2].value,
-      species: inputs[3].value,
-      type: inputs[4].value,
-      gender: inputs[5].value,
-      location: inputs[6].value,
-      isFavourite: true,
-    } as ProtoCharacterStructure;
+  // const handleSubmit = (event: SyntheticEvent) => {
+  //   event.preventDefault();
+  //   const form = event.target as HTMLFormElement;
+  //   const inputs = form.querySelectorAll("input");
+  //   const newCharacter: ProtoCharacterStructure | CharacterStructure = {
+  //     image: inputs[0].value,
+  //     name: inputs[1].value,
+  //     status: inputs[2].value,
+  //     species: inputs[3].value,
+  //     type: inputs[4].value,
+  //     gender: inputs[5].value,
+  //     location: inputs[6].value,
+  //     isFavourite: true,
+  //   } as ProtoCharacterStructure;
 
-    if (type === "add") {
-      addChar(newCharacter);
-    } else {
-      (newCharacter as CharacterStructure).id = (char as CharacterStructure).id;
-      updateChar(newCharacter as CharacterStructure);
-    }
+  //   if (type === "add") {
+  //     addChar(newCharacter);
+  //   } else {
+  //     (newCharacter as CharacterStructure).id = (char as CharacterStructure).id;
+  //     updateChar(newCharacter as CharacterStructure);
+  //   }
   };
 
   return (
