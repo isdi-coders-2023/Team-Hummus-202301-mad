d4db942e160fdac2e1c64106b4d09a21120f8aab
Jimena <jimenanrp2@gmail.com>
2023-02-20 20:05:40 +0100

    Erase unused declarations

--- src/components/detail/detail.tsx
+++ src/components/detail/detail.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { Link, useParams } from "react-router-dom";
 import { AppContext } from "../../context/context public/context";
 
