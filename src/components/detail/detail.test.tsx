277e8f6647f031d4257c8169c12c1754b8d55984
Jimena <jimenanrp2@gmail.com>
2023-02-20 20:03:46 +0100

    Change tests

--- src/components/detail/detail.test.tsx
+++ src/components/detail/detail.test.tsx
@@ -1,18 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter as Router } from "react-router-dom";
-import { DetailCardStructure } from "../../models/cards/card";
 import { Detail } from "./detail";
-const mockDetailCard: DetailCardStructure = {
-  char: "string",
-} as unknown as DetailCardStructure;
 
 describe("Given a character detail component", () => {
   describe("when it is rendered", () => {
     test("then it should list items", () => {
       render(
         <Router>
-          DetailPage
-          <Detail char={mockDetailCard}></Detail>
+          <Detail></Detail>
         </Router>
       );
       const element = screen.getAllByRole("img");
